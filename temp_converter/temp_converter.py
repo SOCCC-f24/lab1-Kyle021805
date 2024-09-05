583c6f3771235da807ea3217acf67398d3af5caf
Kyle021805 <105394177+Kyle021805@users.noreply.github.com>
2024-09-05 16:58:52 -0400

    Update temp_converter.py
    Edited the formula

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -2,11 +2,11 @@
 
 # process
 def c2f(c):
-    return c * 9 / 5 + 32
+    return (c*(9/5) + 32)
 
 def main(cel):
     return c2f(cel)
 
 if __name__ == "__main__":
-    cel = 100         # input
+    cel = 0         # input
     print(main(cel))  # output
